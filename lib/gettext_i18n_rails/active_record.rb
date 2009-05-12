277f011b9db7e76dd1840b91a7fca4541e48455d
grosser <grosser.michael@gmail.com>
2009-05-12 09:12:13 +0200

    fix to better work with strange calls to human name, thx to Marc-Andre

--- lib/gettext_i18n_rails/active_record.rb
+++ lib/gettext_i18n_rails/active_record.rb
@@ -1,11 +1,11 @@
 class ActiveRecord::Base
   # CarDealer.sales_count -> s_('CarDealer|Sales count') -> 'Sales count' if no translation was found
-  def self.human_attribute_name(attribute)
+  def self.human_attribute_name(attribute, *args)
     s_(gettext_translation_for_attribute_name(attribute))
   end
 
   # CarDealer -> _('car dealer')
-  def self.human_name
+  def self.human_name(*args)
     _(self.to_s.underscore.gsub('_',' '))
   end
 
