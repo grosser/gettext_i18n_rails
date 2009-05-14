e541c30211da6284f0d61cc2b3dd262859a5be64
grosser <grosser.michael@gmail.com>
2009-05-14 09:15:51 +0200

    defer loading so that rake gems:install can install fast_gettext, thanks to matthuhiggins

--- init.rb
+++ init.rb
@@ -4,4 +4,6 @@ rescue LoadError
   # weird bug, when run with rake rails reports error that session
   # store is not configured, this fixes it somewhat...
 end
-require 'gettext_i18n_rails'
+
+#requires fast_gettext to be present, so give rails a chance to install it using rake gems:install
+config.after_initialize do require 'gettext_i18n_rails' end
