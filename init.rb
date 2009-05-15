2f5d364d0e3c73dbe60e205201967ca84f583fb0
grosser <grosser.michael@gmail.com>
2009-05-15 20:18:34 +0200

    rake gems:install also works without config.after_initialize

--- init.rb
+++ init.rb
@@ -5,5 +5,4 @@ rescue LoadError
   # store is not configured, this fixes it somewhat...
 end
 
-#requires fast_gettext to be present, so give rails a chance to install it using rake gems:install
-config.after_initialize do require 'gettext_i18n_rails' end
+require 'gettext_i18n_rails'
