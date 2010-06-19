3866025e4a4c9ea91c556b9664eba03bc7445a35
J. Pablo Fernández <pupeno@pupeno.com>
2010-06-19 14:40:12 +0200

    Workaround for not correctly setting I18n.config.locale. More about it on http://github.com/grosser/fast_gettext/issues/#issue/5

--- lib/gettext_i18n_rails/i18n_hacks.rb
+++ lib/gettext_i18n_rails/i18n_hacks.rb
@@ -3,7 +3,16 @@ module I18n
   # this is not chainable, since FastGettext may reject this locale!
   def locale=(new_locale)
     FastGettext.locale = new_locale
+
+    # Between Rails 2.3.5 and 2.3.8 the way the current locale is maintained changed, now there's a config object
+    # keeping the state. This is a quick hack and should probably be cleaned. For more information read this bug report:
+    # http://github.com/grosser/fast_gettext/issues/#issue/5
+    # Maybe a better solution should be found, something like conditional chaining.
+    if FastGettext.locale.to_sym == new_locale.to_sym && self.respond_to?(:config)
+      config.locale = new_locale
+    end
   end
+
   def locale
     FastGettext.locale.to_sym
   end
