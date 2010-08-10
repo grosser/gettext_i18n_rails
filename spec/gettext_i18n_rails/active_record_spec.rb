5ae0f340312e0df419aaa6ca533662009d4522f6
grosser <grosser.michael@gmail.com>
2010-08-10 20:31:48 +0200

    add spec for missing (but obtrusive) feature compared to GetText

--- spec/gettext_i18n_rails/active_record_spec.rb
+++ spec/gettext_i18n_rails/active_record_spec.rb
@@ -23,30 +23,45 @@ describe ActiveRecord::Base do
     FastGettext.current_cache = {}
   end
 
-  it "has a human name that is translated through FastGettext" do
-    CarSeat.should_receive(:_).with('car seat').and_return('Autositz')
-    CarSeat.human_name.should == 'Autositz'
+  describe :human_name do
+    it "is translated through FastGettext" do
+      CarSeat.should_receive(:_).with('car seat').and_return('Autositz')
+      CarSeat.human_name.should == 'Autositz'
+    end
   end
 
-  it "translates attributes through FastGettext" do
-    CarSeat.should_receive(:s_).with('CarSeat|Seat color').and_return('Sitz farbe')
-    CarSeat.human_attribute_name(:seat_color).should == 'Sitz farbe'
+  describe :human_attribute_name do
+    it "translates attributes through FastGettext" do
+      CarSeat.should_receive(:s_).with('CarSeat|Seat color').and_return('Sitz farbe')
+      CarSeat.human_attribute_name(:seat_color).should == 'Sitz farbe'
+    end
   end
 
-  it "translates error messages" do
-    FastGettext.stub!(:current_repository).and_return('translate me'=>"Übersetz mich!")
-    FastGettext._('translate me').should == "Übersetz mich!"
-    c = CarSeat.new
-    c.valid?
-    c.errors.on(:seat_color).should == "Übersetz mich!"
-  end
+  describe 'error messages' do
+    let(:model){
+      c = CarSeat.new
+      c.valid?
+      c
+    }
 
-  it "translates scoped error messages" do
-    pending 'scope is no longer added in 3.x' if ActiveRecord::VERSION::MAJOR >= 3 
-    FastGettext.stub!(:current_repository).and_return('activerecord.errors.translate me'=>"Übersetz mich!")
-    FastGettext._('activerecord.errors.translate me').should == "Übersetz mich!"
-    c = CarSeat.new
-    c.valid?
-    c.errors.on(:seat_color).should == "Übersetz mich!"
+    it "translates error messages" do
+      FastGettext.stub!(:current_repository).and_return('translate me'=>"Übersetz mich!")
+      FastGettext._('translate me').should == "Übersetz mich!"
+      model.errors.on(:seat_color).should == "Übersetz mich!"
+    end
+
+    it "translates scoped error messages" do
+      pending 'scope is no longer added in 3.x' if ActiveRecord::VERSION::MAJOR >= 3
+      FastGettext.stub!(:current_repository).and_return('activerecord.errors.translate me'=>"Übersetz mich!")
+      FastGettext._('activerecord.errors.translate me').should == "Übersetz mich!"
+      model.errors.on(:seat_color).should == "Übersetz mich!"
+    end
+
+    it "translates error messages with %{fn}" do
+      pending
+      FastGettext.stub!(:current_repository).and_return('translate me'=>"Übersetz %{fn} mich!")
+      FastGettext._('translate me').should == "Übersetz %{fn} mich!"
+      model.errors.on(:seat_color).should == "Übersetz car_seat mich!"
+    end
   end
 end
