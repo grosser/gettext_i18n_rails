6317f3a3d5b6ebf46264c9f90582139405638b72
grosser <grosser.michael@gmail.com>
2011-09-25 19:42:27 +0200

    fix template locale hack in abstractcontroller

--- lib/gettext_i18n_rails/i18n_hacks.rb
+++ lib/gettext_i18n_rails/i18n_hacks.rb
@@ -1,38 +1,25 @@
+I18n::Config # autoload
+
 module I18n
-  module_function
+  class Config
+    def locale
+      FastGettext.locale.to_sym
+    end
 
-  def locale=(new_locale)
-    FastGettext.locale = new_locale
-  end
-
-  def locale
-    FastGettext.locale.to_sym
-  end
-
-  # since Rails 2.3.8 a config object is used instead of just .locale
-  if defined? Config
-    class Config
-      def locale
-        FastGettext.locale.to_sym
-      end
-
-       def locale=(new_locale)
-        FastGettext.locale=(new_locale)
-      end
+     def locale=(new_locale)
+      FastGettext.locale=(new_locale)
     end
   end
 
   # backport I18n.with_locale if it does not exist
-  unless respond_to?(:with_locale)
-    # Executes block with given I18n.locale set.
-    def with_locale(tmp_locale = nil)
-      if tmp_locale
-        current_locale = self.locale
-        self.locale = tmp_locale
-      end
-      yield
-    ensure
-      self.locale = current_locale if tmp_locale
+  # Executes block with given I18n.locale set.
+  def self.with_locale(tmp_locale = nil)
+    if tmp_locale
+      current_locale = self.locale
+      self.locale = tmp_locale
     end
-  end
+    yield
+  ensure
+    self.locale = current_locale if tmp_locale
+  end unless defined? I18n.with_locale
 end
