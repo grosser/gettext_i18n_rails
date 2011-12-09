24ab830de450658a9212bff8dcb2fbfb1c472da2
Paul McMahon <paul@mobalean.com>
2011-12-09 13:55:28 +0900

    Make msgmerge configurable and better defaults
    --no-location is better for source control, as line number changes are
    not constantly being marked as changes in .po files
    
    --no-wrap mac/linux have a 1 char different default width, so this
    ensures no changes when finding on different systems

--- lib/gettext_i18n_rails/railtie.rb
+++ lib/gettext_i18n_rails/railtie.rb
@@ -2,6 +2,8 @@
 if defined?(Rails::Railtie)
   module GettextI18nRails
     class Railtie < ::Rails::Railtie
+      config.gettext_i18n_rails = ActiveSupport::OrderedOptions.new
+      config.gettext_i18n_rails.msgmerge = %w[--sort-output --no-location --no-wrap]
       rake_tasks do
         require 'gettext_i18n_rails/tasks'
       end
