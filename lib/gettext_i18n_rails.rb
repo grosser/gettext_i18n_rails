d0036e5b4d351fc53cc12baf6fd7ff3ffa9edac9
Seb Bacon <seb.bacon@gmail.com>
2012-06-14 10:45:28 +0100

    Revert "include "Translation" in active record, even when Railtie is defined."
    This reverts commit 6806bc8ebfbec5c14811644a63ab82d955f74edb.

--- lib/gettext_i18n_rails.rb
+++ lib/gettext_i18n_rails.rb
@@ -29,11 +29,9 @@ if not defined?(Rails::Railtie) and defined?(ActiveRecord)
   ActiveRecord::Base.extend GettextI18nRails::ActiveRecord
 end
 
-if defined?(ActiveModel)
+if not defined?(Rails::Railtie) and defined?(ActiveModel)
+  require 'gettext_i18n_rails/active_model'
   ActiveRecord::Base.extend ActiveModel::Translation
-  if not defined?(Rails::Railtie)
-      require 'gettext_i18n_rails/active_model'
-  end
 end
 
 require 'gettext_i18n_rails/action_controller' if defined?(ActionController) # so that bundle console can work in a rails project
