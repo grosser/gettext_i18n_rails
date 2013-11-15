060dcddd46a5d079fb2d1b9db6a65dc0782ea590
Paul McMahon <paul@doorkeeper.jp>
2013-11-15 17:34:16 +0900

    Set up reloading after initialization
    Otherwise fastgettext might not yet be configured

--- lib/gettext_i18n_rails/railtie.rb
+++ lib/gettext_i18n_rails/railtie.rb
@@ -14,7 +14,12 @@ module GettextI18nRails
       end
     end
 
-    initializer "gettext_i18n_rails.callbacks" do |app|
+    config.after_initialize do |app|
+      if app.config.gettext_i18n_rails.use_for_active_record_attributes
+        ActiveSupport.on_load :active_record do
+          require 'gettext_i18n_rails/active_model'
+        end
+      end
       repo = FastGettext.translation_repositories[FastGettext.text_domain]
       if repo.is_a? FastGettext::TranslationRepository::Po
         reloader = ActiveSupport::FileUpdateChecker.new([], FastGettext.locale_path => :po) do
@@ -23,14 +28,6 @@ module GettextI18nRails
         app.reloaders << reloader
         ActionDispatch::Reloader.to_prepare do
           reloader.execute
-        end
-      end
-    end
-
-    config.after_initialize do |app|
-      if app.config.gettext_i18n_rails.use_for_active_record_attributes
-        ActiveSupport.on_load :active_record do
-          require 'gettext_i18n_rails/active_model'
         end
       end
     end
