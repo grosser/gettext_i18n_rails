ecee7c25b13ea9550d865282afbceff3307a4cb2
Nathan Ehresman <n_github@ehresman.org>
2019-12-10 16:14:50 -0500

    DRY - should have done it this way to begin with

--- lib/gettext_i18n_rails/action_controller.rb
+++ lib/gettext_i18n_rails/action_controller.rb
@@ -1,18 +1,7 @@
 # Autoloading in initializers is deprecated on rails 6.0.  This delays initialization using the on_load
 # hooks, but does not change behaviour for existing rails versions.
-if defined?(Rails) && Rails::VERSION::MAJOR >= 6
-  ActiveSupport.on_load(:action_controller_base) do
-    class ::ActionController::Base
-      def set_gettext_locale
-        requested_locale = params[:locale] || session[:locale] || cookies[:locale] ||  request.env['HTTP_ACCEPT_LANGUAGE'] || I18n.default_locale
-        locale = FastGettext.set_locale(requested_locale)
-        session[:locale] = locale
-        I18n.locale = locale # some weird overwriting in action-controller makes this necessary ... see I18nProxy
-      end
-    end
-  end
-else
-  class ActionController::Base
+path_controller = ->() {
+  class ::ActionController::Base
     def set_gettext_locale
       requested_locale = params[:locale] || session[:locale] || cookies[:locale] ||  request.env['HTTP_ACCEPT_LANGUAGE'] || I18n.default_locale
       locale = FastGettext.set_locale(requested_locale)
@@ -20,4 +9,11 @@ else
       I18n.locale = locale # some weird overwriting in action-controller makes this necessary ... see I18nProxy
     end
   end
+}
+if defined?(Rails) && Rails::VERSION::MAJOR >= 6
+  ActiveSupport.on_load(:action_controller_base) do
+    path_controller.call
+  end
+else
+  path_controller.call
 end
